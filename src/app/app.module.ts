7e86aded1da46c14d7198b1e53a229be65864159
Nora <aishah.azmi@terang.asia>
2019-04-17 10:31:19 +0800

    Install Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {NgbAlertModule, NgbModule, NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SharedModule,
     NgbModule,
     BrowserModule,
     NgbPaginationModule,
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+
+export const BOOTSTRAP_MODULE = [];
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class SharedModule { }
